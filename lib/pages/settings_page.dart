d05a64c1338259fd576e90ef7cfb001051707cd8
hanifahfz <hanifahfaizah13@gmail.com>
2026-06-03 22:04:45 +0700

    fitur: menambahkan pilihan COD di checkout

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:reusea/services/auth_service.dart';
+import 'login_page.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
